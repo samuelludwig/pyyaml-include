c9c7d982d20df27a16fc3b29447472fb47c39c71
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-11-02 12:25:20 -0400

    Remove reference to non-existant version file

--- src/yamlinclude/__init__.py
+++ src/yamlinclude/__init__.py
@@ -6,4 +6,4 @@ Include YAML files within YAML
 
 from .constructor import *  # noqa
 from .readers import *  # noqa
-from .version import version as __version__  # noqa
+#from .version import version as __version__  # noqa
